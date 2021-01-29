7472de6837fef5dbb5640a972f9724a3baa34c5d
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-01-29 22:53:36 +0100

    Expanded functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import unittest, time
 
 
 class NewVisitor(unittest.TestCase):
@@ -16,19 +17,33 @@ class NewVisitor(unittest.TestCase):
 
         # The website title suggests it's another boring todo list app.
         self.assertIn('To-Do', self.browser.title)
-
-        # This will just fail and remind you to finish this test!
-        self.fail('Finish the test buddy!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is welcomed with a window to enter a list item.
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to do item'
+        )
+        
         # User types in "feed my cat" to a textbox.
+        inputbox.send_keys('feed my cat')
 
         # User submits the item by hitting enter,
         # the list item is now displayed on the website.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: feed my cat' for row in rows)
+        )
 
         # The textbox is still there so user can enter another item.
-
+        self.fail('Finish the test bud!')
+        
         # When user added his first item do the list it's now preserved
         # under a unique URL.
 
