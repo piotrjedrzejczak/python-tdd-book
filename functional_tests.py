ccfbc38226d494ed300f5b175cd85b7f2e4401c6
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-01-30 21:14:47 +0100

    FT helper function

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitor(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retreive_it_later(self):
         # User navigates to the main page of the website.
         self.browser.get('http://localhost:8000')
@@ -34,10 +39,7 @@ class NewVisitor(unittest.TestCase):
         # the list item is now displayed on the website.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: feed my cat', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: feed my cat')
 
         # The textbox is still there so user can enter another item.
         self.fail('Finish the test bud!')
