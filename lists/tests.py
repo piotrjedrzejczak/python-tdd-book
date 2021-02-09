a6619b9a369e3281722560201449376959e2f76d
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-02-09 23:07:02 +0100

    another stepping stone for the personalized list delivery

--- lists/tests.py
+++ lists/tests.py
@@ -10,21 +10,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-    
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-    
-    def test_redirect_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-url')
 
 
 class ItemModelTest(TestCase):
@@ -61,3 +46,16 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-url/')
         self.assertTemplateUsed(response, 'list.html')
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_redirect_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-url/')
