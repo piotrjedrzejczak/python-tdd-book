efb53fa23fbcb4606433e0c8b619830185b73f2d
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-02-10 20:28:40 +0100

    preping the setup for staging server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
+import os
 
 
 MAX_WAIT_TIME = 10
@@ -13,6 +14,9 @@ class NewVisitor(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
