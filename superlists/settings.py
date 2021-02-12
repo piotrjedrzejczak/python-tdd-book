1d92b05c381759f8efa16e38d5d53f403d092f0d
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-02-12 23:25:38 +0100

    env vars for prod and dev

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6_nuc69_1=+)bblhahow#9u00nl#9&g+k)!a27m)*#(6r%ba&)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure_dev_key'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
