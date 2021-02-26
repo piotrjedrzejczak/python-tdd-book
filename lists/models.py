d3c46827b52415af815c472d114209cf167d5935
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-02-26 12:52:05 +0100

    duplicate items validation at db layer

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 from django.core.urlresolvers import reverse
 
@@ -11,3 +12,8 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
+    class Meta:
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
